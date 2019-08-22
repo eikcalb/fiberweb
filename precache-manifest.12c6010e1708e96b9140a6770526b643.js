97d9c1da8cd5e5d3f0af4b7ea9ab327531bdaf5e
Agwa Israel Onome <agwaisraelonome@gmail.com>
2019-08-22 06:31:02 +0100

    initial commit from local

---
+++ precache-manifest.12c6010e1708e96b9140a6770526b643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b5fbda111b35e4fb99b4b25a816423",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c84cec63670254fe40be",
+    "url": "/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "ed57affed6c855af47ff",
+    "url": "/static/css/main.05b66a98.chunk.css"
+  },
+  {
+    "revision": "c84cec63670254fe40be",
+    "url": "/static/js/2.ecf8f770.chunk.js"
+  },
+  {
+    "revision": "ed57affed6c855af47ff",
+    "url": "/static/js/main.1eaa64af.chunk.js"
+  },
+  {
+    "revision": "ebfdcc5712655e9641bb",
+    "url": "/static/js/runtime~main.39113e58.js"
+  }
+]);
