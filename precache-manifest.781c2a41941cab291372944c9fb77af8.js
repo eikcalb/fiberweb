bc176b0fb02f2e4c3295ca992b24b4f4f01367e8
Agwa Israel Onome <agwaisraelonome@gmail.com>
2019-09-03 00:54:29 +0100

    initial commit

---
+++ precache-manifest.781c2a41941cab291372944c9fb77af8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8726a58f701923974219bbcf0c4cbfbd",
+    "url": "/fiberweb/index.html"
+  },
+  {
+    "revision": "6e544ae17d8c605042e1",
+    "url": "/fiberweb/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "c43256bf768a199b61ee",
+    "url": "/fiberweb/static/css/main.05b66a98.chunk.css"
+  },
+  {
+    "revision": "6e544ae17d8c605042e1",
+    "url": "/fiberweb/static/js/2.a3ffa452.chunk.js"
+  },
+  {
+    "revision": "c43256bf768a199b61ee",
+    "url": "/fiberweb/static/js/main.ff9f28ae.chunk.js"
+  },
+  {
+    "revision": "cecb7b3c6f2ff53b382e",
+    "url": "/fiberweb/static/js/runtime~main.69f00ca2.js"
+  }
+]);
